5d58e5f6f6840b7f17813ba897b6204823b15b65
ΛAZ27 <laz2727@gmail.com>
2012-12-03 14:31:11 +0600

    Моар фиксов. Ну а хуле.

--- mod_forcefield.java
+++ mod_forcefield.java
@@ -3,7 +3,7 @@ package net.minecraft.src;
 public class mod_forcefield extends BaseMod
 	{
 	public static Block ffield = new BlockFfield(1240, 0).setBlockUnbreakable().setHardness(6000000F).setLightValue(0.4F).setBlockName("ffield");
-	public static Block irtnk = new BlockIrTnk(1241, 0).setBlockUnbreakable().setHardness(6000000F).setLightValue(0.4F).setBlockName("ffield");
+	public static Block irtnk = new BlockIrTnk(1241, 0).setBlockUnbreakable().setHardness(6000000F).setLightValue(0.4F).setBlockName("irtnk");
 	public String getVersion()
         	{
         	return "0.1 alpha";
@@ -11,6 +11,7 @@ public class mod_forcefield extends BaseMod
 	public void load()
 		{	
 		ModLoader.registerBlock(ffield);
+		ModLoader.registerBlock(irtnk);
 		ModLoader.addName(ffield, "Force Field");
 		ModLoader.addName(irtnk, "Iron Tank");
 		ModLoader.addRecipe(new ItemStack(ffield, 64), new Object[]{ "111", "202", "111", Character.valueOf('1'), Block.glass, Character.valueOf('0'), Block.bedrock, Character.valueOf('2'), Item.redstone});	
