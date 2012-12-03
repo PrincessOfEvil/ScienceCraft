8d5817d049c5c96566a0f73269f85ad3dfaee2b3
ΛAZ27 <laz2727@gmail.com>
2012-12-03 14:19:00 +0600

    Новый блок. Пока не имеет смысла.

---
+++ BlockIrTnk.java
@@ -0,0 +1,48 @@
+package net.minecraft.src;
+
+import java.util.Random;
+
+public class BlockIrTnk extends Block
+	{
+	public BlockIrTnk(int i, int j)
+		{
+		super(i, j, Material.glass);
+                                this.setCreativeTab(CreativeTabs.tabBlock);
+		}
+	public int idDropped(int i, Random random, int j)
+		{
+		return mod_forcefield.irtnk.blockID;
+		}
+	public int quantityDropped(Random random)
+		{
+		return 1;
+		}
+		
+	public boolean isOpaqueCube()
+		{
+			return false;
+		}
+	public boolean renderAsNormalBlock()
+		{
+		return false;
+		}
+	public int getRenderBlockPass()
+		{
+		return 1;
+		}
+
+	public boolean shouldSideBeRendered(IBlockAccess iblockaccess, int i, int j, int k, int l)
+		{
+		int i1 = iblockaccess.getBlockId(i, j, k);
+		if(!localFlag && i1 == blockID)
+			{
+			return false;
+			} else
+			{
+			return super.shouldSideBeRendered(iblockaccess, i, j, k, l);
+			}
+        }
+
+	private boolean localFlag;
+
+	}
--- mod_forcefield.java
+++ mod_forcefield.java
@@ -2,8 +2,8 @@ package net.minecraft.src;
 
 public class mod_forcefield extends BaseMod
 	{
-	public static Block ffield = new BlockFfield(240, 0).setBlockUnbreakable().setHardness(6000000F).setLightValue(0.4F).setBlockName("ffield");
-
+	public static Block ffield = new BlockFfield(1240, 0).setBlockUnbreakable().setHardness(6000000F).setLightValue(0.4F).setBlockName("ffield");
+	public static Block irtnk = new BlockIrTnk(1241, 0).setBlockUnbreakable().setHardness(6000000F).setLightValue(0.4F).setBlockName("ffield");
 	public String getVersion()
         	{
         	return "0.1 alpha";
@@ -12,7 +12,9 @@ public class mod_forcefield extends BaseMod
 		{	
 		ModLoader.registerBlock(ffield);
 		ModLoader.addName(ffield, "Force Field");
+		ModLoader.addName(irtnk, "Iron Tank");
 		ModLoader.addRecipe(new ItemStack(ffield, 64), new Object[]{ "111", "202", "111", Character.valueOf('1'), Block.glass, Character.valueOf('0'), Block.bedrock, Character.valueOf('2'), Item.redstone});	
 		ffield.blockIndexInTexture = ModLoader.addOverride("/terrain.png", "/lazmod/ffield.png");
+		irtnk.blockIndexInTexture = ModLoader.addOverride("/terrain.png", "/lazmod/iron_tank.png")
 		}
 	}
